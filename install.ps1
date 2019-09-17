7b50940de87dbd717aec3e4cd78768112408dce1
Florian Mautendorfer <florian.mautendorfer@gmail.com>
2019-09-17 11:20:30 +0200

    added initial powershell install script

---
+++ install.ps1
@@ -0,0 +1,5 @@
+Invoke-Command {reg import .\regfix.reg *>&1 | Out-Null}
+Invoke-WebRequest Invoke-WebRequest "https://www.python.org/ftp/python/3.7.4/python-3.7.4.exe" -OutFile ".\python-install.exe"
+python-install.exe /quiet InstallAllUsers=1 PrependPath=1
+Invoke-Command {pip install selenium}
+Invoke-Expression "cmd.exe /C https://chromedriver.chromium.org/downloads"
