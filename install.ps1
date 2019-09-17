2db70d1607c797db458f5f7db4b48ca5a655ad47
Florian Mautendorfer <florian.mautendorfer@gmail.com>
2019-09-17 11:22:41 +0200

    added https config

--- install.ps1
+++ install.ps1
@@ -1,3 +1,5 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
 Invoke-Command {reg import .\regfix.reg *>&1 | Out-Null}
 Invoke-WebRequest Invoke-WebRequest "https://www.python.org/ftp/python/3.7.4/python-3.7.4.exe" -OutFile ".\python-install.exe"
 python-install.exe /quiet InstallAllUsers=1 PrependPath=1
