cbfa7c60a1bf7ac144d4a158e03e3e5aeda7ccc1
Florian Mautendorfer <florian.mautendorfer@gmail.com>
2019-09-18 19:22:43 +0200

    added ignore for the downloaded python installer and completed the powershell installer

--- install.ps1
+++ install.ps1
@@ -1,7 +1,9 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 Invoke-Command {reg import .\regfix.reg *>&1 | Out-Null}
-Invoke-WebRequest Invoke-WebRequest "https://www.python.org/ftp/python/3.7.4/python-3.7.4.exe" -OutFile ".\python-install.exe"
-python-install.exe /quiet InstallAllUsers=1 PrependPath=1
+if (-Not (Test-Path -Path '.\python-install.exe')) {
+    Invoke-WebRequest "https://www.python.org/ftp/python/3.7.4/python-3.7.4.exe" -OutFile ".\python-install.exe"
+}
+Invoke-Command {.\python-install.exe /quiet InstallAllUsers=1 PrependPath=1}
 Invoke-Command {pip install selenium}
-Invoke-Expression "cmd.exe /C https://chromedriver.chromium.org/downloads"
+Invoke-Command {cmd.exe /C start https://chromedriver.chromium.org/downloads}
